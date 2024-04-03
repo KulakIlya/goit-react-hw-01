1bc33bc06e7f9de6ed89df803d514fc005c6ac17
Ilya <kulak1224@gmail.com>
2024-04-03 12:12:38 +0300

    adding whole hw

---
+++ src/components/FriendList/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,21 @@
+import clsx from 'clsx';
+import PropTypes from 'prop-types';
+import styles from './FriendListItem.module.css';
+
+const FriendListItem = ({ avatar, name, isOnline }) => {
+  const status = clsx(styles.status, { [styles.online]: isOnline });
+  return (
+    <div>
+      <img src={avatar} alt={name} width="48" />
+      <p className={styles.name}>{name}</p>
+      <p className={status}>{isOnline ? 'Online' : 'Offline'}</p>
+    </div>
+  );
+};
+export default FriendListItem;
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string,
+  name: PropTypes.string,
+  isOnline: PropTypes.bool,
+};
